a9244d330a77359cc4dfd4699af3798a70e34f53
angha1234 <98279734+angha1234@users.noreply.github.com>
2024-01-25 15:08:29 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,64 @@
+from tkinter import *
+from PIL import ImageTk,Image
+import pymysql
+from tkinter import messagebox
+from AddBook import *
+from DeleteBook import *
+from ViewBooks import *
+from IssueBook import *
+from ReturnBook import *
+mypass = "MyNewPass"
+mydatabase="db"
+
+con = pymysql.connect(host="localhost",user="root",password=mypass,database=mydatabase)
+cur = con.cursor()
+
+root = Tk()
+root.title("LIBRARY MANAGEMENT SYSTEM")
+root.minsize(width=600,height=600)
+root.geometry("700x700")
+
+same=True
+n=0.25
+
+background_image =Image.open("lib.jpg")
+[imageSizeWidth, imageSizeHeight] = background_image.size
+
+newImageSizeWidth = int(imageSizeWidth*n)
+if same:
+    newImageSizeHeight = int(imageSizeHeight*n) 
+else:
+    newImageSizeHeight = int(imageSizeHeight/n)
+    
+    
+background_image = background_image.resize((newImageSizeWidth,newImageSizeHeight),Image.ANTIALIAS)
+img = ImageTk.PhotoImage(background_image)
+
+Canvas1 = Canvas(root)
+
+Canvas1.create_image(300,340,image = img)      
+Canvas1.config(bg="white",width = newImageSizeWidth, height = newImageSizeHeight)
+Canvas1.pack(expand=True,fill=BOTH)
+
+headingFrame1 = Frame(root,bg="#FFBB00",bd=5)
+headingFrame1.place(relx=0.2,rely=0.1,relwidth=0.6,relheight=0.16)
+
+headingLabel = Label(headingFrame1, text="WELCOME TO \n ANGHA AND ASWINI LIBRARY", bg='black', fg='white', font=('Courier',15))
+headingLabel.place(relx=0,rely=0, relwidth=1, relheight=1)
+
+btn1 = Button(root,text="ADD BOOK DETAILS",bg='black', fg='white', command=addBook)
+btn1.place(relx=0.28,rely=0.4, relwidth=0.45,relheight=0.1)
+    
+btn2 = Button(root,text="DELETE BOOK",bg='black', fg='white', command=delete)
+btn2.place(relx=0.28,rely=0.5, relwidth=0.45,relheight=0.1)
+    
+btn3 = Button(root,text="VIEW BOOK LIST",bg='black', fg='white', command=View)
+btn3.place(relx=0.28,rely=0.6, relwidth=0.45,relheight=0.1)
+    
+btn4 = Button(root,text="ISSUE BOOK TO STUDENT",bg='black', fg='white', command = issueBook)
+btn4.place(relx=0.28,rely=0.7, relwidth=0.45,relheight=0.1)
+    
+btn5 = Button(root,text="RETURN BOOK",bg='black', fg='white', command = returnBook)
+btn5.place(relx=0.28,rely=0.8, relwidth=0.45,relheight=0.1)
+
+root.mainloop()
